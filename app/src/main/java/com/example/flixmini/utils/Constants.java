dde59d5b05da2f7b264d1dc1ec5416ef71c4ef9d
123 <bethan828@gmail.com>
2019-07-08 21:48:06 +0300

    interface

--- app/src/main/java/com/example/flixmini/utils/Constants.java
+++ app/src/main/java/com/example/flixmini/utils/Constants.java
@@ -2,6 +2,6 @@ package com.example.flixmini.utils;
 
 public interface Constants {
     String BASE_URL = "https://api.themoviedb.org/3/";
-    String API_KEY = "";
+    String API_KEY = "6ccd72a2a8fc239b13f209408fc31c33";
     String LANGUAGE = "ru-RU";
 }
